2a6b5916f0394da5b7a53940ce8fc221478cf2c4
Zer0553 <124603918+Zer0553@users.noreply.github.com>
2023-09-19 16:48:21 +0400

    Update lab9.py

--- lab9.py
+++ lab9.py
@@ -27,18 +27,20 @@ class Registr:
                     con.commit()
                     msg = "Новый пользователь успешно зарегестрирован"
                     mb.showinfo("Успешно", msg)
+                    self.window_Reg.destroy()
 
     def __init__(self):
-        window_Reg = Tk()
-        window_Reg.title('Регистрация')
-        window_Reg.geometry('300x300')
-        username_label = Label(window_Reg, text='Имя пользователя', )
-        username_entry = Entry(window_Reg, bg='#fff', fg='#444')
-        password_label = Label(window_Reg, text='Пароль')
-        password_entry = Entry(window_Reg, bg='#fff', fg='#444')
-        password_label_confirm = Label(window_Reg, text='Повторите пароль')
-        password_entry_confirm = Entry(window_Reg, bg='#fff', fg='#444')
-        send_btn = Button(window_Reg, text='Зарегистрироваться', command=lambda:
+        self.window_Reg = Tk()
+        self.window_Reg.title('Регистрация')
+        self.window_Reg.geometry('300x300')
+        self.window_Reg.eval('tk::PlaceWindow . center')
+        username_label = Label(self.window_Reg, text='Имя пользователя', )
+        username_entry = Entry(self.window_Reg, bg='#fff', fg='#444')
+        password_label = Label(self.window_Reg, text='Пароль')
+        password_entry = Entry(self.window_Reg, bg='#fff', fg='#444')
+        password_label_confirm = Label(self.window_Reg, text='Повторите пароль')
+        password_entry_confirm = Entry(self.window_Reg, bg='#fff', fg='#444')
+        send_btn = Button(self.window_Reg, text='Зарегистрироваться', command=lambda:
         self.CreateNewUser(username_entry.get(), password_entry.get(), password_entry_confirm.get(), ))
 
         username_label.pack(padx=10, pady=8)
@@ -49,7 +51,7 @@ class Registr:
         password_entry_confirm.pack(padx=10, pady=8)
         send_btn.pack(padx=10, pady=8)
 
-        window_Reg.mainloop()
+        self.window_Reg.mainloop()
 
 
 class Login_in:
@@ -68,7 +70,8 @@ class Login_in:
                     if password == i[1]:
                         msg = "Вы успешно зашли"
                         mb.showinfo("Успешно", msg)
-                        return
+                        self.window_Login.destroy()
+                        return 
                     else:
                         msg = 'Пароль не совпадает'
                         mb.showerror("Ошибка", msg)
@@ -80,14 +83,15 @@ class Login_in:
 
 
     def __init__(self):
-        window_Login = Tk()
-        window_Login.title('Вход')
-        window_Login.geometry('300x250')
-        username_label = Label(window_Login, text='Имя пользователя', )
-        username_entry = Entry(window_Login, bg='#fff', fg='#444')
-        password_label = Label(window_Login, text='Пароль')
-        password_entry = Entry(window_Login, bg='#fff', fg='#444')
-        send_btn = Button(window_Login, text='Войти', command=lambda:
+        self.window_Login = Tk()
+        self.window_Login.title('Вход')
+        self.window_Login.geometry('300x250')
+        self.window_Login.eval('tk::PlaceWindow . center')
+        username_label = Label(self.window_Login, text='Имя пользователя', )
+        username_entry = Entry(self.window_Login, bg='#fff', fg='#444')
+        password_label = Label(self.window_Login, text='Пароль')
+        password_entry = Entry(self.window_Login, bg='#fff', fg='#444')
+        send_btn = Button(self.window_Login, text='Войти', command=lambda:
         self.CheckExist(username_entry.get(), password_entry.get()))
 
         username_label.pack(padx=10, pady=8)
@@ -96,13 +100,14 @@ class Login_in:
         password_entry.pack(padx=10, pady=8)
         send_btn.pack(padx=10, pady=8)
 
-        window_Login.mainloop()
+        self.window_Login.mainloop()
 
 
 class App:
     window_Entry = Tk()
     window_Entry.title('Lab 9')
     window_Entry.geometry('250x250')
+    window_Entry.eval('tk::PlaceWindow . center')
     loginin = Button(window_Entry, text='Вход', command=Login_in)
     reg = Button(window_Entry, text='Регистрация', command=Registr)
     loginin.pack(padx=10, pady=8)
